99cec5f5aae7401078886cbaab9ac502d2f68800
Anup Krishna Kumar <akumar11@mt.iitr.ac.in>
2021-04-12 16:31:26 +0530

    images edited

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='https://miro.medium.com/max/14720/1*1QXHs9_GXpO1HqFQBbYHAQ.jpeg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='https://cdn.wallpapersafari.com/65/17/8Jsk7w.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='https://images.unsplash.com/photo-1533495720786-6dade9109c5d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTM0MjUzMHx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=750&q=60'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='https://dergi.halimiz.com/wp-content/uploads/2020/03/john-o-nolan-o_gJAkcKJmM-unsplash.jpg'
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='https://miro.medium.com/max/6300/0*aiDI0NiXsAwFLN1-'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,10 +3,12 @@ import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
 
+// /videos/video-1.mp4
+
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      {/* <video src='./' autoPlay loop muted /> */}
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
